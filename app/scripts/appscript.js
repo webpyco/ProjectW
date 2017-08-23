4ea4925e8d1f9dfee03c75687f11e2d24ff8d69f
Daniel Dominguez <dominguezdaniel@live.com>
2017-08-23 10:59:11 -0500

    Adding App Link
    Adding Files and App Link

---
+++ app/scripts/appscript.js
@@ -0,0 +1,14 @@
+// Script for displaying input values //
+
+/*
+$('.input1').keyup(function(event) {
+  newText = event.target.value;
+  $('.appform').text(newText);
+});
+
+$('.input2').keyup(function(event) {
+  newText = event.target.value;
+  $('.appform2').text(newText);
+});
+
+*/
